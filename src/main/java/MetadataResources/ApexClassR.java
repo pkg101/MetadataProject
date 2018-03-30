051eac13332e31350f0c0f2e77282e58be24b69c
pradeepj18 <info.mca2016@gmail.com>
2018-03-30 15:00:22 +0530

    make it better

--- src/main/java/MetadataResources/ApexClassR.java
+++ src/main/java/MetadataResources/ApexClassR.java
@@ -25,17 +25,31 @@ public class ApexClassR {
 	@Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
 	public List<ApexClass> appexClass()
 	{
-		JSONArray apexclassArray = DataWarehouse.getApexClassList(loginObject,startdate,enddate);
+		JSONArray customFieldList = DataWarehouse.__getCustomFieldList(loginObject,startdate,enddate);
 		List<ApexClass> apexClassList = new ArrayList<ApexClass>();
 		try {
-			
-		for (int i = 0; i < apexclassArray.length(); i++) {
+			String customObjectName = "";
+			for (int j = 0; j < customFieldList.length(); j++) {
+				if (customFieldList.getJSONObject(j).getString("TableEnumOrId").matches("^[A-Za-z]+[0-9]*"))
+					customObjectName = customFieldList.getJSONObject(j).getString("TableEnumOrId");
+				else {
+					customObjectName = DataWarehouse.getCustomObjectName(loginObject,
+							customFieldList.getJSONObject(j).getString("TableEnumOrId"));
+					customObjectName += "__c";
+				}
+				ApexClass apexClass = new ApexClass();
+				apexClass.setName(customObjectName + "."+ customFieldList.getJSONObject(j).getString("DeveloperName") + "__c");
+				System.out.println(apexClass.getName());
+				apexClassList.add(apexClass);
+				
+				}
+		/*for (int i = 0; i < apexclassArray.length(); i++) {
 			ApexClass apexClass = new ApexClass();
 		//	apexClass.setId(apexclassArray.getJSONObject(i).getString("Id"));
 			apexClass.setName(apexclassArray.getJSONObject(i).getString("Name"));
 		//	apexClass.setLastModifiedDate(apexclassArray.getJSONObject(i).getString("LastModifiedDate"));
 			apexClassList.add(apexClass);	
-		}
+		}*/
 		
 		} catch (Exception e) {
 			e.printStackTrace();
